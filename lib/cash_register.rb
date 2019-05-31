7c44235e95f80baaf6e741748c28c45624f7dc18
poignant-minifier-0983 <davidfano@gmail.com>
2019-05-31 19:26:06 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+  attr_accessor :total, :discount, :add_item
 
+  def initialize(discount=0)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price, increase)
+    @title = title
+    @price = price
+    @increase = increase
+  end
+
+end
